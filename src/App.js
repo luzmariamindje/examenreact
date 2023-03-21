a68d6d9d1777aa0617e32b1265e665cc5804f752
lmindje <luzmariamindje@estudiante.edib.es>
2023-03-21 19:01:41 +0100

    commit examen

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hooks from './Componentes/Hooks';
+
+function App() {
+  return (
+    <div className="App">
+     <Hooks initialNumber={0}></Hooks>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí es donde se realizaría la validación y la autenticación del usuario
+    console.log('Submit', username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+export default LoginForm
